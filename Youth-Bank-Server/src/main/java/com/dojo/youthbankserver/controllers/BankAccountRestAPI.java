d8ae1e9c5c89e3c700a19768717984ffb9fd6a01
Lazherjouili <lazher.jouili.job@gmail.com>
2024-02-20 22:56:10 +0100

    response

--- Youth-Bank-Server/src/main/java/com/dojo/youthbankserver/controllers/BankAccountRestAPI.java
+++ Youth-Bank-Server/src/main/java/com/dojo/youthbankserver/controllers/BankAccountRestAPI.java
@@ -1,7 +1,11 @@
+
 package com.dojo.youthbankserver.controllers;
 
 import java.util.List;
 
+import com.dojo.youthbankserver.dtos.*;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dojo.youthbankserver.dtos.AccountHistoryDTO;
-import com.dojo.youthbankserver.dtos.AccountOperationDTO;
-import com.dojo.youthbankserver.dtos.BankAccountDTO;
-import com.dojo.youthbankserver.dtos.CreditDTO;
-import com.dojo.youthbankserver.dtos.DebitDTO;
-import com.dojo.youthbankserver.dtos.TransferRequestDTO;
 import com.dojo.youthbankserver.exceptions.BalanceNotSufficientException;
 import com.dojo.youthbankserver.exceptions.BankAccountNotFoundException;
 import com.dojo.youthbankserver.services.BankAccountService;
@@ -28,49 +26,52 @@ import com.dojo.youthbankserver.services.BankAccountService;
 
 public class BankAccountRestAPI {
 
-	  private BankAccountService bankAccountService;
+    private BankAccountService bankAccountService;
 
 
-	    @GetMapping("/{accountId}")
-	    public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
-	        return bankAccountService.getBankAccount(accountId);
-	    }
-	    
-	    
-	    
-	    @GetMapping("")
-	    public List<BankAccountDTO> listAccounts(){
-	        return bankAccountService.bankAccountList();
-	    }
-	    
-	    
-	    @GetMapping("/{accountId}/operations")
-	    public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
-	        return bankAccountService.accountHistory(accountId);
-	    }
+    @GetMapping("/{accountId}")
+    public ResponseEntity<BankAccountDTO> getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
+        return ResponseEntity.ok().body(bankAccountService.getBankAccount(accountId));
+    }
 
-	    @GetMapping("/{accountId}/pageOperations")
-	    public AccountHistoryDTO getAccountHistory(
-	            @PathVariable String accountId,
-	            @RequestParam(name="page",defaultValue = "0") int page,
-	            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
-	        return bankAccountService.getAccountHistory(accountId,page,size);
-	    }
-	    @PostMapping("/debit")
-	    public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
-	        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
-	        return debitDTO;
-	    }
-	    @PostMapping("/credit")
-	    public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
-	        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
-	        return creditDTO;
-	    }
-	    @PostMapping("/transfer")
-	    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
-	        this.bankAccountService.transfer(
-	                transferRequestDTO.getAccountSource(),
-	                transferRequestDTO.getAccountDestination(),
-	                transferRequestDTO.getAmount());
-	    }
+
+
+    @GetMapping("")
+    public ResponseEntity<List<BankAccountDTO>> listAccounts(){
+        return ResponseEntity.ok().body(bankAccountService.bankAccountList());
+    }
+    @PostMapping("/saving/customerId")
+    public CheckingBankAccountDTO saveCheckingBankAccountDTO(@Valid @RequestBody @PathVariable String customerId ){
+        return bankAccountService.saveCheckingBankAccount();
+    }
+
+    @GetMapping("/{accountId}/operations")
+    public ResponseEntity<List<AccountOperationDTO>> getHistory(@PathVariable String accountId){
+        return ResponseEntity.ok().body(bankAccountService.accountHistory(accountId));
+    }
+
+    @GetMapping("/{accountId}/pageOperations")
+    public ResponseEntity<AccountHistoryDTO> getAccountHistory(
+            @PathVariable String accountId,
+            @RequestParam(name="page",defaultValue = "0") int page,
+            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
+        return ResponseEntity.ok().body(bankAccountService.getAccountHistory(accountId,page,size));
+    }
+    @PostMapping("/debit")
+    public ResponseEntity<DebitDTO> debit(@Valid @RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        return ResponseEntity.ok().body(debitDTO);
+    }
+    @PostMapping("/credit")
+    public ResponseEntity<CreditDTO> credit(@Valid @RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
+        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
+        return ResponseEntity.ok().body(creditDTO);
+    }
+    @PostMapping("/transfer")
+    public void transfer(@Valid @RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.transfer(
+                transferRequestDTO.getAccountSource(),
+                transferRequestDTO.getAccountDestination(),
+                transferRequestDTO.getAmount());
+    }
 }
